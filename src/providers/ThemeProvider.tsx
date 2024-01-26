eb089e43050a5bd9bc50dab317dc8a9fa3747b55
Rodrigo Rocha de Queiroz <rodrigo@emperium.com.br>
2024-01-26 15:15:59 -0300

    Update: import alias added in theme provider

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider as StyledThemeProvider } from 'styled-components'
-import { defaultTheme } from '../styles/themes/default'
+import { defaultTheme } from '@/styles/themes/default'
 
 interface ThemeProviderProps {
   children: React.ReactNode
